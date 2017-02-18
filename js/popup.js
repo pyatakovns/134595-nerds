ecb39446a7bfa73c5695f7dfae52a6f4bd947c82
pyatakovns <pyatakovns@mail.ru>
2017-02-18 13:12:56 +0300

    после финальной проверки исправление замечаний

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+var link = document.querySelector(".write-us");
+        var popup = document.querySelector(".modal-content");
+        var close = document.querySelector(".modal-content-close");
+
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            popup.classList.add("modal-content-show");
+        });
+
+        close.addEventListener("click", function(event) {
+            event.preventDefault();
+            popup.classList.remove("modal-content-show");
+        });
